9ee497703458d45aa0c5172ba6e432be6bee07d1
deciek <luongvukhoa572001@gmail.com>
2023-08-18 13:44:28 +0700

    1

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+// import SearchResults from "@/components/SearchResults";
+import Header from "@/Components/Header";
+import Test from "./test";
+const index = () =>{
+  return(
+    <div>
+      <Header />
+    </div>
+  )
+}
+export default index;
---
+++ pages/test.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Test() {
+  return (
+   
+    <p className='text-yellow-600'>helloasdasdas</p>
+  )
+}
+
